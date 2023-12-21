a28e8d85ecbbec386636d68e1aa9a07c094ddfad
Tobias Hort-Giess <tobias.hort-giess@compass-security.com>
2023-12-21 14:22:21 +0100

    action refactoring

--- WysiwygEditor.java
+++ WysiwygEditor.java
@@ -55,7 +55,6 @@ public class WysiwygEditor extends JTextPane implements Editor {
 
       addMouseListener(followHyperlinks());
     
-
       getInputMap().put(KeyStrokes.CTRL_B, new StyledEditorKit.BoldAction());
       getInputMap().put(KeyStrokes.CTRL_I, new StyledEditorKit.ItalicAction());
       getInputMap().put(KeyStrokes.CTRL_U, new StyledEditorKit.UnderlineAction());
@@ -87,21 +86,27 @@ public class WysiwygEditor extends JTextPane implements Editor {
             "<div class=\"warn\"></div>", 
             HTML.Tag.BODY, 
             HTML.Tag.DIV));
+      getInputMap().put(
+          KeyStrokes.CTRL_SHIFT_U, 
+          new HTMLEditorKit.InsertHTMLTextAction(
+            "InsertUnorderedListAction", 
+            "<ul><li></li></ul>", 
+            HTML.Tag.BODY, 
+            HTML.Tag.UL));
+      getInputMap().put(
+          KeyStrokes.CTRL_SHIFT_O, 
+          new HTMLEditorKit.InsertHTMLTextAction(
+            "InsertOrderedListAction", 
+            "<ol><li></li></ol>", 
+            HTML.Tag.BODY, 
+            HTML.Tag.OL));
+      getInputMap().put(KeyStrokes.CTRL_SHIFT_T, new InsertTableAction());
+      getInputMap().put(KeyStrokes.CTRL_V, new PasteAction());
 
-      registerKeyboardAction(pasteFromClipboard(), KeyStrokes.CTRL_V, WHEN_FOCUSED);
-      //registerKeyboardAction(continueNext(), KeyStrokes.ENTER, WHEN_FOCUSED);
-      //registerKeyboardAction(insertBreak(), KeyStrokes.SHIFT_ENTER, WHEN_FOCUSED);
-      registerKeyboardAction(insertUnorderedList(), KeyStrokes.CTRL_SHIFT_U, WHEN_FOCUSED);
-      registerKeyboardAction(insertOrderedList(), KeyStrokes.CTRL_SHIFT_O, WHEN_FOCUSED);
-      registerKeyboardAction(insertTable(), KeyStrokes.CTRL_SHIFT_T, WHEN_FOCUSED);
-      registerKeyboardAction(indent(), KeyStrokes.TAB, WHEN_FOCUSED);
-      registerKeyboardAction(unindent(), KeyStrokes.SHIFT_TAB, WHEN_FOCUSED);
-      registerKeyboardAction(toParagraph(), KeyStrokes.CTRL_0, WHEN_FOCUSED);
       registerKeyboardAction(save(), KeyStrokes.CTRL_S, WHEN_FOCUSED);
       registerKeyboardAction(refresh(), KeyStrokes.CTRL_R, WHEN_FOCUSED);
       registerKeyboardAction(undo(undoManager), KeyStrokes.CTRL_Z, WHEN_FOCUSED);
       registerKeyboardAction(redo(undoManager), KeyStrokes.CTRL_Y, WHEN_FOCUSED);
-      //registerKeyboardAction(backspace(), KeyStrokes.BACKSPACE, WHEN_FOCUSED);
       registerKeyboardAction(debug(), KeyStrokes.F12, WHEN_FOCUSED);
     } catch (Exception exc) {
       throw new RuntimeException(exc);
@@ -123,220 +128,6 @@ public class WysiwygEditor extends JTextPane implements Editor {
 
   public boolean hasUnsavedChanges() {
     return unsavedChangesTracker.hasUnsavedChanges();
-  }
-
-  private void resetCaret() {
-    setCaretPosition(getCaretPosition());
-  }
-
-  private ActionListener pasteFromClipboard() {
-    return event -> {
-      try {
-        var content = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
-        if (content == null) {
-          return;
-        }
-        if(!content.isDataFlavorSupported(DataFlavor.imageFlavor)) {
-          var string = (String) content.getTransferData(DataFlavor.stringFlavor);
-          getDocument().insertString(getCaretPosition(), string, null);
-          return;
-        }
-        var image = (BufferedImage) content.getTransferData(DataFlavor.imageFlavor);
-        var attachments = Paths.get(Config.attachments());
-        var imageFileName = note.getFullName() + "_" + System.currentTimeMillis() + ".png";
-        var imageFile = Paths.get(attachments.toString(), imageFileName);
-        ImageIO.write(image, "png", imageFile.toFile());
-        var html = "<p><img src=\"../attachments/"+imageFileName+"\"></p>";
-        addToBody(html);
-      } catch (Exception exc) {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private ActionListener insertUnorderedList() {
-    return event ->  {
-      addToBody("<ul><li></li></ul>");
-    };
-  }
-  
-  private ActionListener insertOrderedList() {
-    return event ->  {
-      addToBody("<ol><li></li></ol>");
-    };
-  }
-
-  private ActionListener insertBreak() {
-    return event -> {
-      try {
-        var document = (HTMLDocument) getDocument();
-        var editorKit = (HTMLEditorKit) getEditorKit();
-        editorKit.insertHTML(document, getCaretPosition(), "<br>", 0, 0, HTML.Tag.BR);
-        resetCaret();
-      } catch (Exception exc) {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private ActionListener insertInfoBlock() {
-    return event -> {
-      addToBody("<div class=\"info\"><p></p></div>");
-    };
-  }
-  
-  private ActionListener insertWarnBlock() {
-    return event -> {
-      addToBody("<div class=\"warn\"><p></p></div>");
-    };
-  }
-
-  private ActionListener insertCodeBlock() {
-    return event -> {
-      try {
-        var document = (HTMLDocument) getDocument();
-        var editorKit = (HTMLEditorKit) getEditorKit();
-        var selectedText = getSelectedText();
-        if (selectedText != null && !selectedText.isBlank()) {
-          document.remove(getSelectionStart(), selectedText.length());
-          var html = "<code>" + selectedText + "</code>";
-          editorKit.insertHTML(document, getCaretPosition(), html, 0, 0, HTML.Tag.CODE);
-          resetCaret();
-        } else {
-          addToBody("<pre><code></code></pre>");
-        }
-      } catch (Exception exc) {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private ActionListener insertTable() {
-    return event -> {
-      var colSpinner = new JSpinner(new SpinnerNumberModel(2, 0, 100, 1));
-      var rowSpinner = new JSpinner(new SpinnerNumberModel(2, 0, 100, 1));
-      
-      Object[] message = {
-        "Columns:", colSpinner,
-        "Rows:   ", rowSpinner
-      };
-
-      var option = 
-        JOptionPane.showConfirmDialog(
-            App.instance(), 
-            message, 
-            "Insert table", 
-            JOptionPane.OK_CANCEL_OPTION,
-            JOptionPane.PLAIN_MESSAGE);
-
-      if (option != JOptionPane.OK_OPTION) {
-        return;
-      }
-
-      var cols = (int) colSpinner.getValue();
-      var rows = (int) rowSpinner.getValue();
-     
-      var sb = new StringBuilder();
-      sb.append("<table>");
-      sb.append("<thead>");
-      sb.append("<tr>");
-      for (int col = 0; col < cols; col++) {
-        sb.append("<th></th>");
-      }
-      sb.append("</tr>");
-      sb.append("</thead>");
-      sb.append("<tbody>");
-      for (int row = 0; row < rows; row++) {
-        sb.append("<tr>");
-        for (int col = 0; col < cols; col++) {
-          sb.append("<td></td>");
-        }
-        sb.append("</tr>");
-      }
-      sb.append("</tbody>");
-      sb.append("</table>");
-
-      addToBody(sb.toString());
-    };
-  }
-
-  private ActionListener continueNext() {
-    return event -> {
-      try {
-        var document = (HTMLDocument) getDocument();
-        var element = document.getParagraphElement(getCaretPosition()).getParentElement();
-        if (element != null) {
-          var text = getElementText(element);
-          if ("li".equals(getElementName(element))) {
-            if (!text.isBlank()) {
-              document.insertAfterEnd(element, "<li></li>");
-              setCaretPosition(element.getEndOffset());
-              return;
-            } else {
-              document.removeElement(element);
-            }
-          }
-        }
-        addToBody("<p></p>");
-      } catch (Exception exc)  {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private String getElementName(Element element) {
-    return element.getAttributes().getAttribute(AttributeSet.NameAttribute).toString();
-  }
-
-  private String getElementText(Element element) {
-    try {
-      var document = (HTMLDocument) getDocument();
-      var length = element.getEndOffset() - element.getStartOffset();
-      return document.getText(element.getStartOffset(), length);
-    } catch (Exception exc) {
-      throw new RuntimeException(exc);
-    }
-  }
-
-  private void addToBody(String html) {
-    try {
-      var document = (HTMLDocument) getDocument();
-      var element = document.getParagraphElement(getCaretPosition());
-      while (!"body".equals(getElementName(element.getParentElement()))) {
-        element = element.getParentElement();
-      }
-      document.insertAfterEnd(element, html);
-      setCaretPosition(element.getEndOffset());
-    } catch (Exception exc) {
-      throw new RuntimeException(exc);
-    }
-  }
-
-  private ActionListener toParagraph() {
-    return event -> {
-      var document = (HTMLDocument) getDocument();
-      var element = document.getParagraphElement(getCaretPosition());
-      var parent = element.getParentElement();
-      if (!"body".equals(getElementName(parent))) {
-        return;
-      }
-      replaceTag("<p>", "</p>");
-    };
-  }
-
-  private ActionListener toHeading(int level) {
-    return event -> replaceTag("<h" + level + ">", "</h" + level + ">");
-  }
-
-  private void replaceTag(String startTag, String endTag) {
-    try {
-      var document = (HTMLDocument) getDocument();
-      var element = document.getParagraphElement(getCaretPosition());
-      var elementText = getElementText(element);
-      document.setOuterHTML(element, startTag + elementText + endTag);
-    } catch (Exception exc) {
-      throw new RuntimeException(exc);
-    }
   }
 
   private ActionListener save() {
@@ -373,88 +164,6 @@ public class WysiwygEditor extends JTextPane implements Editor {
         undoManager.redo();
       } catch (CannotRedoException exc) {
       }
-    };
-  }
-
-  private ActionListener indent() {
-    return event -> {
-      try {
-        var document = (HTMLDocument) getDocument();
-        var position = getCaretPosition();
-        var element = document.getParagraphElement(position).getParentElement();
-        if (element != null && "li".equals(getElementName(element))) {
-          var parent = element.getParentElement();
-          var listTag = getElementName(parent);
-          var sb = new StringBuilder();
-          var iterator = new ElementIterator(element);
-          Element current = null;
-          while ((current = iterator.next()) != null) {
-            switch (getElementName(current)) {
-              case "content" -> sb.append(getElementText(current));
-              case "li" -> sb.append("<li>");
-              case "ul" -> sb.append("<ul>");
-              case "ol" -> sb.append("<ol>");
-            }
-          }
-          for(var index = 0; index < parent.getElementCount(); index++) {
-            if (parent.getElement(index) == element && index > 0) {
-              var previous = parent.getElement(index - 1);
-              if (previous.getElementCount() > 1) {
-                document.insertBeforeEnd(previous.getElement(1), sb.toString());
-              } else {
-                document.insertBeforeEnd(previous, "<%s>%s</%s>".formatted(listTag, sb.toString(), listTag));
-              }
-              document.removeElement(element);
-              break;
-            }
-          }
-          setCaretPosition(position);
-        } else {
-          getActionMap().get("insert-tab").actionPerformed(event);
-        }
-      } catch (Exception exc) {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private ActionListener unindent() {
-    return event -> {
-      try {
-        var document = (HTMLDocument) getDocument();
-        var position = getCaretPosition();
-        var element = document.getParagraphElement(position).getParentElement();
-        if (element != null && "li".equals(getElementName(element))) {
-          var parent = element.getParentElement();
-          var listTag = getElementName(parent);
-          var parentOfParent = parent.getParentElement();
-          if (parentOfParent != null && "li".equals(getElementName(parentOfParent))) {
-            var sb = new StringBuilder();
-            var iterator = new ElementIterator(element);
-            Element current = null;
-            while ((current = iterator.next()) != null) {
-              switch (getElementName(current)) {
-                case "content" -> sb.append(getElementText(current));
-                case "li" -> sb.append("<li>");
-                case "ul" -> sb.append("<ul>");
-                case "ol" -> sb.append("<ol>");
-              }
-            }
-            document.insertBeforeEnd(parentOfParent.getParentElement(), sb.toString());
-            document.removeElement(element);
-            setCaretPosition(position);
-          }
-        }
-      } catch (Exception exc) {
-        throw new RuntimeException(exc);
-      }
-    };
-  }
-
-  private ActionListener backspace() {
-    return event -> {
-      getActionMap().get("delete-previous").actionPerformed(event);
-      resetCaret();
     };
   }
 
@@ -551,6 +260,98 @@ public class WysiwygEditor extends JTextPane implements Editor {
         editorKit.insertHTML(document, position, "<br>", 0, 0, HTML.Tag.BR);
       } catch (Exception exc) {
         throw new RuntimeException();
+      }
+    }
+  }
+
+  class InsertTableAction extends HTMLTextAction {
+    public InsertTableAction() {
+      super("InsertTableAction");
+    }
+
+    public void actionPerformed(ActionEvent event) {
+      var colSpinner = new JSpinner(new SpinnerNumberModel(2, 0, 100, 1));
+      var rowSpinner = new JSpinner(new SpinnerNumberModel(2, 0, 100, 1));
+      
+      Object[] message = {
+        "Columns:", colSpinner,
+        "Rows:   ", rowSpinner
+      };
+
+      var option = 
+        JOptionPane.showConfirmDialog(
+            App.instance(), 
+            message, 
+            "Insert table", 
+            JOptionPane.OK_CANCEL_OPTION,
+            JOptionPane.PLAIN_MESSAGE);
+
+      if (option != JOptionPane.OK_OPTION) {
+        return;
+      }
+
+      var cols = (int) colSpinner.getValue();
+      var rows = (int) rowSpinner.getValue();
+     
+      var sb = new StringBuilder();
+      sb.append("<table>");
+      sb.append("<thead>");
+      sb.append("<tr>");
+      for (int col = 0; col < cols; col++) {
+        sb.append("<th></th>");
+      }
+      sb.append("</tr>");
+      sb.append("</thead>");
+      sb.append("<tbody>");
+      for (int row = 0; row < rows; row++) {
+        sb.append("<tr>");
+        for (int col = 0; col < cols; col++) {
+          sb.append("<td></td>");
+        }
+        sb.append("</tr>");
+      }
+      sb.append("</tbody>");
+      sb.append("</table>");
+
+      new HTMLEditorKit.InsertHTMLTextAction(
+          "InsertTableAction", 
+          sb.toString(), 
+          HTML.Tag.BODY, 
+          HTML.Tag.TABLE).actionPerformed(event);
+    }
+  }
+
+  class PasteAction extends HTMLTextAction {
+    public PasteAction() {
+      super("PasteAction");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+      try {
+        var content = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+        if (content == null) {
+          return;
+        }
+        if(!content.isDataFlavorSupported(DataFlavor.imageFlavor)) {
+          var string = (String) content.getTransferData(DataFlavor.stringFlavor);
+          var editor = getEditor(event);
+          var document = getHTMLDocument(editor);
+          document.insertString(editor.getCaretPosition(), string, null);
+          return;
+        }
+        var image = (BufferedImage) content.getTransferData(DataFlavor.imageFlavor);
+        var attachments = Paths.get(Config.attachments());
+        var imageFileName = note.getFullName() + "_" + System.currentTimeMillis() + ".png";
+        var imageFile = Paths.get(attachments.toString(), imageFileName);
+        ImageIO.write(image, "png", imageFile.toFile());
+        new HTMLEditorKit.InsertHTMLTextAction(
+            "PasteAction", 
+            "<p><img src=\"../attachments/" + imageFileName + "\"></p>", 
+            HTML.Tag.BODY, 
+            HTML.Tag.P).actionPerformed(event);
+      } catch (Exception exc) {
+        throw new RuntimeException(exc);
       }
     }
   }
